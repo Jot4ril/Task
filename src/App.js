7c2290232acc9ebb358f27fc7b6ec9a086f97c98
unknown <jamesthomasogar@gmail.com>
2022-10-17 12:33:03 +0100

    server task

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+
+function App() {
+  const Greeting = "Hello World";
+  return <div className="App">{Greeting} </div>;
+}
+console.log("Hello World");
+
+export default App;
---
+++ src/server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const port = 3000;
+
+// Create a server object:
+const server = http.createServer(function (req, res) {
+  // Write a response to the client
+  res.write("<h1>Hello Node!!!!</h1>\n");
+
+  // End the response
+  res.end();
+});
+
+// Set up our server so it will listen on the port
+server.listen(port, function (error) {
+  // Checking any error occur while listening on port
+  if (error) {
+    console.log("Something went wrong", error);
+  }
+  // Else sent message of listening
+  else {
+    console.log("Server is listening on port" + port);
+  }
+});
